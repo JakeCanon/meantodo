4527bbe3ebb5a46e0045d113705762c671281b4f
JakeCanon <Montecowboy@gmail.com>
2017-11-20 13:42:26 -0500

    conected to my database using mlab hosting service

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://robin:robin@ds113626.mlab.com:13626/meantodos', ['todos']);
 
 router.get('/todos', function(req, res, next){
-        res.send('TODOS API');
+        db.todos.find(function(err, todos){
+                if(err){
+                        res.send(err);
+                }else {
+                        res.json(todos);
+                }
+        });
 });
 
 module.exports = router;
